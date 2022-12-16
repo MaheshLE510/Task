8e48fcd4871a6bc3c748f2a10d2f95f3046401d6
asus <asus@MM18371>
2022-12-16 11:38:14 +0530

    Error

--- src/main/java/Devops/Prime/App.java
+++ src/main/java/Devops/Prime/App.java
@@ -23,7 +23,7 @@ public class App {
        for(int i=2;i<=num/2;i++)
        {
            if((num%i)==0)
-               return  false;
+               return  false
        }
        return true;
     }
